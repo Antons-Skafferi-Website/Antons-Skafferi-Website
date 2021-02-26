291278f4859672c91c9222f29f294db962d57844
feny1800 <feny1800@student.miun.se>
2021-02-26 12:50:18 +0100

    reservation backend
    table id is of right now hard coded in the insert statement.

--- src/main/java/beans/Reservation.java
+++ src/main/java/beans/Reservation.java
@@ -7,9 +7,16 @@ package beans;
 
 import classes.Database;
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+//import java.util.Date;
+import java.sql.Date;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+
 
 /**
  *
@@ -21,11 +28,28 @@ public class Reservation implements Serializable{
     private final Database connection = new Database();
     
     private int numberOfGuests;
+    private int tableID;
     private String name;
-    private String date;
+    private Date date;
     private String phone;
     private String time;
     private String email;
+    private String comment;
+
+    public void setTableID(int tableID) {
+        this.tableID = tableID;
+    }
+
+    public int getTableID() {
+        return tableID;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
 
     public void setNumberOfGuests(int numberOfGuests) {
         this.numberOfGuests = numberOfGuests;
@@ -35,7 +59,7 @@ public class Reservation implements Serializable{
         this.name = name;
     }
 
-    public void setDate(String date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
@@ -59,7 +83,7 @@ public class Reservation implements Serializable{
         return name;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
@@ -75,14 +99,18 @@ public class Reservation implements Serializable{
         return email;
     }
     
-    public String makeReservation() {
-       if(connection.makeReservation(numberOfGuests, name, email))
+    public void makeReservation() throws ParseException {
+       if(connection.makeReservation(numberOfGuests, name, email, date, time, comment))
        {
-           return "Bokning genomförd!";
+            FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage("Bokning genomförd!","The reservation was made successfully"));
        }
        else
        {
-           return "Något gick fel, försök igen.";
+            FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage("Något gick fel, försök igen","something went wrong during the reservation to the database"));
        }
         
     }
--- src/main/java/classes/Database.java
+++ src/main/java/classes/Database.java
@@ -1,6 +1,6 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template file, choose Tools | Templatess
  * and open the template in the editor.
  */
 package classes;
@@ -8,12 +8,18 @@ package classes;
 
 import beans.Dish;
 import java.sql.Connection;
+import java.sql.Date;
+//import java.time.LocalDate;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -31,8 +37,8 @@ public class Database {
     "WHERE SMHD.SUB_MENU_ID = ?";
     
     private String insertReservation = "INSERT INTO APP.RESERVATION " + 
-    "(CUSTOMER_NAME, CUSTOMER_EMAIL, CUSTOMER_COUNT, DATE_TIME, COMMENT)\n" +
-    "VALUES(?, ?, ?, '2020-02-24 17:30:00.000', 'vill så och äta')";
+    "(CUSTOMER_NAME, CUSTOMER_EMAIL, CUSTOMER_COUNT, RESERVATION_DATE, RESERVATION_TIME, TABLE_ID, COMMENT)\n" +
+    "VALUES(?, ?, ?, ?, ?, 1, ?)";
     
     //int numberOfGuests, String name, String date,  String email
     public Database() {
@@ -62,8 +68,16 @@ public class Database {
         return list;
     }
     
-    public boolean makeReservation(int numberOfGuests, String name, String email) {
+    public boolean makeReservation(int numberOfGuests, String name, String email, Date date, String time, String comment) throws ParseException {
         System.out.println("name: " + name);
+        HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String temp = request.getParameter("date");
+        System.out.println("date: " + temp);
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date parsed = format.parse(temp);
+        java.sql.Date sqlDate = new java.sql.Date(parsed.getTime());
+        System.out.println("datesql: " + sqlDate);
         try (Connection connection = DriverManager.getConnection(dbURL, user, pw);
             PreparedStatement ps = connection.prepareCall(insertReservation);
         ){
@@ -71,6 +85,9 @@ public class Database {
             ps.setString(1, name);
             ps.setString(2, email);
             ps.setInt(3, numberOfGuests);
+            ps.setDate(4, sqlDate);
+            ps.setString(5, time);
+            ps.setString(6, comment);
             ps.execute();
             return true;
         }catch(SQLException e) {
